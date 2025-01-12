3995ff408e2272ef42c5e7b10dcfe90173043907
leandrodalbo <leandro.edbo@gmail.com>
2025-01-12 12:19:37 +0000

    project started

---
+++ src/main/java/com/crypto/kraken/bot/KrakenBotApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.kraken.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenBotApplication.class, args);
+	}
+
+}
